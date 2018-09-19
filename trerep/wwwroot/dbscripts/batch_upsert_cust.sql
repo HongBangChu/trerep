c23469d3a2e78a1fddf9aba82e88bf770a47683d
Nguyễn Hồng Minh <nhminh@live.com>
2018-09-20 00:40:26 +0700

    todo: move css in para.cshtml to site.css, post data to model, model call batch_upsert_cust

---
+++ trerep/wwwroot/dbscripts/batch_upsert_cust.sql
@@ -0,0 +1,10 @@
+﻿create or replace function para.batch_upsert_cust(
+    p_jsonstr json,
+    o_norows out integer)
+as $$
+declare
+	v_a integer;
+begin
+	o_norows := 0;
+end;
+$$ LANGUAGE plpgsql;
