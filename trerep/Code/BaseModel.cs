ef3414143233b50c59a410fbf0efe618eac47b92
Nguyễn Hồng Minh <nhminh@live.com>
2018-10-11 23:46:42 +0700

    coding: fx report, create table report data (importing step 2) 1 extract each sheet to one method 2 extract each business to one method

--- trerep/Code/BaseModel.cs
+++ trerep/Code/BaseModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,18 @@ namespace trerep.Code
         protected const string jsonFormat = "{{\"rows\":{0},\"total\":{1}}}";
         protected readonly IConfiguration _configuration;
         protected readonly string _connStr;
+        protected readonly IHostingEnvironment _hostingEnvironment;
         public BaseModel(IConfiguration configuration)
         {
             _configuration = configuration;
             _connStr = _configuration.GetConnectionString("PostgresConnection");
+        }
+
+        public BaseModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            _configuration = configuration;
+            _connStr = _configuration.GetConnectionString("PostgresConnection");
+            _hostingEnvironment = hostingEnvironment;
         }
     }
 }
---
+++ trerep/Pages/Report.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using trerep.Code;
+
+namespace trerep.Pages
+{
+    public class ReportModel : BaseModel
+    {
+        public ReportModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment) : base(configuration, hostingEnvironment) { }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostExport()
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = @"CustomerReport.xlsx";
+            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            var memory = new MemoryStream();
+            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook;
+                workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Thống kê CN");
+                IRow row = excelSheet.CreateRow(0);
+
+                row.CreateCell(0).SetCellValue("Tháng");
+                row.CreateCell(3).SetCellValue("Mã CN");
+
+                row = excelSheet.CreateRow(1);
+                row.CreateCell(0).SetCellValue("Năm");
+
+                row = excelSheet.CreateRow(2);
+                row.CreateCell(3).SetCellValue("Số lượng KH KDVTT");
+                
+                row = excelSheet.CreateRow(3);
+                row.CreateCell(3).SetCellValue("SL KHDN MBNT");
+
+                workbook.Write(fs);
+            }
+            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
+        }
+    }
+}
