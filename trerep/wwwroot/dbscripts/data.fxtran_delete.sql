00b91138977495aa5c2128608181198c822f77f9
Nguyễn Hồng Minh <nhminh@live.com>
2018-10-11 11:17:15 +0700

    delete fxtran

---
+++ trerep/wwwroot/dbscripts/data.fxtran_delete.sql
@@ -0,0 +1,29 @@
+﻿-- FUNCTION: data.fxtran_delete(text)
+
+-- DROP FUNCTION data.fxtran_delete(text);
+
+CREATE OR REPLACE FUNCTION data.fxtran_delete(
+	p_params text,
+	OUT o_result json)
+    RETURNS json
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+AS $BODY$
+
+declare
+    i_tranid integer;
+begin
+	i_tranid := 0;
+	i_tranid := p_params::jsonb->'tranid';
+    delete from fxtran where tranid = i_tranid;
+    o_result := ('{"tranid":'||i_tranid||',"result":"success"}')::json;
+exception when others then
+	o_result := ('{"tranid":'||i_tranid||',"result":"fail","detail":"'||sqlerrm||sqlstate||'"}')::json;
+end;
+
+$BODY$;
+
+ALTER FUNCTION data.fxtran_delete(text)
+    OWNER TO postgres;
--- trerep/wwwroot/js/site.js
+++ trerep/wwwroot/js/site.js
@@ -2,11 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-var selectionActive = function (instance, firstColumn, lastColumn) {
-    // because sheet focus on to A1 before delete then blur after delete
-    if ($(firstColumn).prop('id') != $(lastColumn).prop('id'))
-        $(instance).data('cif', $(firstColumn).text());
-}
 
 var bootpagOpt = {
     page: 1,
