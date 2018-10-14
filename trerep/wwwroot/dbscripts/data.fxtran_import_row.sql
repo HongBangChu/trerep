a1f6909b95190de236543eff6d16ef5eb56b0a37
Nguyễn Hồng Minh <nhminh@live.com>
2018-10-14 23:52:38 +0700

    seperate each report sheet into one razor page

--- trerep/wwwroot/dbscripts/data.fxtran_import_row.sql
+++ trerep/wwwroot/dbscripts/data.fxtran_import_row.sql
@@ -1,6 +1,7 @@
 ﻿CREATE OR REPLACE FUNCTION data.fxtran_import_row(
 	p_params text,
 	OUT o_result json)
+    RETURNS json
     LANGUAGE 'plpgsql'
     COST 100.0
 
@@ -11,15 +12,19 @@ declare
     i_month integer;
     i_year integer;
     i_tranid integer;
+    i_cif integer;
 	json_result jsonb;
 begin
 	json_result := '{}';
 	i_tranid := 0;
-    i_month := to_number(p_params::json->>'month', '99');
-    i_year := to_number(p_params::json->>'year', '9999');
+    -- i_month := to_number(p_params::json->>'month', '99');
+    -- i_year := to_number(p_params::json->>'year', '9999');
     json_row := p_params::json->>'row';
+    i_month := to_number(to_char(to_date(json_row->>1, 'dd/mm/yyyy'), 'mm'), '99');
+    i_year := to_number(to_char(to_date(json_row->>1, 'dd/mm/yyyy'), 'yyyy'), '9999');
 	-- 0: stt, 7: id
     i_tranid := json_row->>7;
+    i_cif := to_number(json_row->>8, '9999999999');
 	INSERT INTO public.fxtran (
 		tranid
 		, ngaygd
@@ -49,7 +54,7 @@ begin
 		, json_row->>4
 		, json_row->>5
 		, json_row->>6
-		, to_number(json_row->>8, '9999999999')
+		, i_cif
 		, json_row->>9
 		, json_row->>10
 		, json_row->>11
@@ -70,7 +75,7 @@ begin
 		, cnthien=json_row->>4
 		, gdv=json_row->>5
 		, ksv=json_row->>6
-		, cif=to_number(json_row->>8, '9999999999')
+		, cif=i_cif
 		, doitac=json_row->>9
 		, loaigd=json_row->>10
 		, ntegd=json_row->>11
@@ -84,7 +89,7 @@ begin
 		, thang=i_month, nam=i_year
 	;
     select ('{"tranid":'||i_tranid||',"result":"success"}')::jsonb into json_result;
-    
+    perform report.fxreport_upsert(i_cif, i_month, i_year);
 	o_result := json_result::json;
 exception when others then
 	select ('{"tranid":'||i_tranid||',"result":"fail","detail":"'||sqlerrm||sqlstate||'"}')::jsonb into json_result;
---
+++ trerep/wwwroot/dbscripts/report.fxreport_upsert.sql
@@ -0,0 +1,28 @@
+﻿CREATE OR REPLACE FUNCTION report.fxreport_upsert(
+	p_cif integer,
+	p_month integer,
+	p_year integer)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100.0
+
+AS $function$
+
+declare
+	d_doanhso numeric;
+    d_loinhuan numeric;
+begin
+	-- hoi lai nghiep vu sum theo truong nao de lay doanh so???
+	select sum(kluqd), sum(lnqv) into d_doanhso, d_loinhuan from fxtran t where t.cif = p_cif and t.thang = p_month and t.nam = p_year;
+    insert into fxreport (cif, doanhso, loinhuan, thang, nam)
+    values (p_cif, d_doanhso, d_loinhuan, p_month, p_year)
+    on conflict (cif, thang, nam)
+	do update
+    	set doanhso = d_doanhso
+        , loinhuan = d_loinhuan;
+end;
+
+$function$;
+
+ALTER FUNCTION report.fxreport_upsert(integer, integer, integer)
+    OWNER TO postgres;
