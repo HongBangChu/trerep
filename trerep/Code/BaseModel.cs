08577a5420ef27038a9b68efd275c5439fab2ba0
Nguyễn Hồng Minh <nhminh@live.com>
2018-11-22 00:19:47 +0700

    refactoring

--- trerep/Code/BaseModel.cs
+++ trerep/Code/BaseModel.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using NPOI.SS.UserModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,6 +28,35 @@ namespace trerep.Code
             _configuration = configuration;
             _connStr = _configuration.GetConnectionString("PostgresConnection");
             _hostingEnvironment = hostingEnvironment;
+        }
+
+        protected string getPostData()
+        {
+            string postData;
+            using (var reader = new StreamReader(Request.Body))
+            {
+                postData = reader.ReadToEnd();
+            }
+            return postData;
+        }
+
+        protected async Task<FileStreamResult> doPostExport(string sFileName, IWorkbook workbook)
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            var memory = new MemoryStream();
+            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+            {
+                workbook.Write(fs);
+            }
+            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            FileStreamResult fsr = File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
+            return fsr;
         }
     }
 }
--- trerep/Pages/Reports/CustByBDS.cshtml.cs
+++ trerep/Pages/Reports/CustByBDS.cshtml.cs
@@ -29,109 +29,93 @@ namespace trerep.Pages.Reports
 
         public async Task<IActionResult> OnPostExport()
         {
-            string postData;
-            using (var reader = new StreamReader(Request.Body))
-            {
-                postData = reader.ReadToEnd();
-            }
-            
-            //var json = new JavaScriptSerializer().Serialize(
-            //                    dict.AllKeys.ToDictionary(k => k, k => dict[k])
-            //           );
-            //ExpandoObject flexible = new ExpandoObject();
-            //foreach (var key in Request.Query.Keys)
-            //{
-            //    string value = Request.Query[key].ToString();
-            //    flexible.AddProperty(key, value);
-            //}
-            string sWebRootFolder = _hostingEnvironment.WebRootPath;
             string sFileName = @"CustomerReport.xlsx";
-            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
-            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
-            var memory = new MemoryStream();
-            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+            IWorkbook workbook = createWorkbook();
+            FileStreamResult fsr = await this.doPostExport(sFileName, workbook);
+            return fsr;
+        }
+
+        private void createSheetHead(ISheet excelSheet, int baseRow)
+        {
+            IRow row = excelSheet.CreateRow(baseRow);
+            row.CreateCell(0).SetCellValue("Tháng");
+            row.CreateCell(3).SetCellValue("Mã CN");
+
+            row = excelSheet.CreateRow(baseRow + 1);
+            row.CreateCell(0).SetCellValue("Năm");
+
+            row = excelSheet.CreateRow(baseRow + 2);
+            row.CreateCell(3).SetCellValue("Số lượng KH KDVTT");
+
+            row = excelSheet.CreateRow(baseRow + 3);
+            row.CreateCell(3).SetCellValue("SL KHDN MBNT");
+            row.CreateCell(6).SetCellValue("SL KHDN IRS");
+
+            row = excelSheet.CreateRow(baseRow + 4);
+            row.CreateCell(3).SetCellValue("SL KHCN MBNT");
+            row.CreateCell(6).SetCellValue("SL KHDN TLHH");
+
+            row = excelSheet.CreateRow(baseRow + 5);
+            row.CreateCell(3).SetCellValue("SL KHDN TDPS");
+            row.CreateCell(6).SetCellValue("SL KHDN CoS");
+
+            row = excelSheet.CreateRow(baseRow + 6);
+            row.CreateCell(3).SetCellValue("SL KHDN CCS");
+            row.CreateCell(6).SetCellValue("SL KHDN DCM");
+        }
+
+        private void createTableHead(ISheet excelSheet, int baseRow, int baseColumn)
+        {
+            IRow row = excelSheet.CreateRow(baseRow);
+            row.CreateCell(baseColumn).SetCellValue("CIF");
+            excelSheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(baseRow, baseRow + 1, baseColumn, baseColumn));
+            row.CreateCell(baseColumn + 1).SetCellValue("Tên KH");
+            excelSheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(baseRow, baseRow + 1, baseColumn + 1, baseColumn + 1));
+            row.CreateCell(baseColumn + 2).SetCellValue("BDS");
+            excelSheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(baseRow, baseRow + 1, baseColumn + 2, baseColumn + 2));
+
+            row.CreateCell(baseColumn + 3).SetCellValue("fx");
+            var cra = new NPOI.SS.Util.CellRangeAddress(baseRow, baseRow, baseColumn + 3, baseColumn + 19);
+            excelSheet.AddMergedRegion(cra);
+        }
+
+        private IWorkbook createWorkbook()
+        {
+            IWorkbook workbook = new XSSFWorkbook();
+            ISheet excelSheet = workbook.CreateSheet("Thống kê CN");
+            int sheetHeadBaseRow = 0;
+            createSheetHead(excelSheet, sheetHeadBaseRow);
+
+            // table head
+            int tableHeadBaseRow = 8, tableHeadBaseColumn = 0;
+            createTableHead(excelSheet, tableHeadBaseRow, tableHeadBaseColumn);
+
+            // table body
+            using (var conn = new NpgsqlConnection(_connStr))
             {
-                IWorkbook workbook;
-                workbook = new XSSFWorkbook();
-                ISheet excelSheet = workbook.CreateSheet("Thống kê CN");
-                IRow row = excelSheet.CreateRow(0);
-
-                row.CreateCell(0).SetCellValue("Tháng");
-                row.CreateCell(3).SetCellValue("Mã CN");
-
-                row = excelSheet.CreateRow(1);
-                row.CreateCell(0).SetCellValue("Năm");
-
-                row = excelSheet.CreateRow(2);
-                row.CreateCell(3).SetCellValue("Số lượng KH KDVTT");
-
-                row = excelSheet.CreateRow(3);
-                row.CreateCell(3).SetCellValue("SL KHDN MBNT");
-                row.CreateCell(6).SetCellValue("SL KHDN IRS");
-
-                row = excelSheet.CreateRow(4);
-                row.CreateCell(3).SetCellValue("SL KHCN MBNT");
-                row.CreateCell(6).SetCellValue("SL KHDN TLHH");
-
-                row = excelSheet.CreateRow(5);
-                row.CreateCell(3).SetCellValue("SL KHDN TDPS");
-                row.CreateCell(6).SetCellValue("SL KHDN CoS");
-
-                row = excelSheet.CreateRow(6);
-                row.CreateCell(3).SetCellValue("SL KHDN CCS");
-                row.CreateCell(6).SetCellValue("SL KHDN DCM");
-
-                // table head
-                row = excelSheet.CreateRow(8);
-                row.CreateCell(0).SetCellValue("CIF");
-                excelSheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(8, 9, 0, 0));
-                row.CreateCell(1).SetCellValue("Tên KH");
-                excelSheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(8, 9, 1, 1));
-                row.CreateCell(2).SetCellValue("BDS");
-                excelSheet.AddMergedRegion(new NPOI.SS.Util.CellRangeAddress(8, 9, 2, 2));
-
-                row.CreateCell(3).SetCellValue("fx");
-                var cra = new NPOI.SS.Util.CellRangeAddress(8, 8, 3, 19);
-                excelSheet.AddMergedRegion(cra);
-
-                // table body
-                using (var conn = new NpgsqlConnection(_connStr))
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("report.customer_get", conn))
                 {
-                    conn.Open();
-                    using (var cmd = new NpgsqlCommand("report.customer_get", conn))
+                    NpgsqlTransaction tran = conn.BeginTransaction();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@p_params", NpgsqlTypes.NpgsqlDbType.Text, this.getPostData().toJsonString());
+
+                    NpgsqlDataReader dr = cmd.ExecuteReader();
+                    int i = 9;
+                    while (dr.Read())
                     {
-                        NpgsqlTransaction tran = conn.BeginTransaction();
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.AddWithValue("@p_params", NpgsqlTypes.NpgsqlDbType.Text, postData.toJsonString());
-                        //NpgsqlParameter outParam = new NpgsqlParameter("@o_rows", NpgsqlTypes.NpgsqlDbType.Refcursor) { Direction = ParameterDirection.Output };
-                        //cmd.Parameters.Add(outParam);
-                        //outParam = new NpgsqlParameter("@o_total", NpgsqlTypes.NpgsqlDbType.Integer) { Direction = ParameterDirection.Output };
-                        //cmd.Parameters.Add(outParam);
-                        //cmd.ExecuteNonQuery();
-
-                        NpgsqlDataReader dr = cmd.ExecuteReader();
-                        int i = 9;
-                        while (dr.Read())
-                        {
-                            row = excelSheet.CreateRow(i);
-                            row.CreateCell(0).SetCellValue(dr["cif"].ToString());
-                            row.CreateCell(1).SetCellValue(dr["name"].ToString());
-                            i++;
-                        }
-                        dr.Close();
-
-                        tran.Commit();
+                        IRow row = excelSheet.CreateRow(i);
+                        row.CreateCell(0).SetCellValue(dr["cif"].ToString());
+                        row.CreateCell(1).SetCellValue(dr["name"].ToString());
+                        i++;
                     }
-                }
+                    dr.Close();
 
-                workbook.Write(fs);
+                    tran.Commit();
+                }
             }
-            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
-            {
-                await stream.CopyToAsync(memory);
-            }
-            memory.Position = 0;
-            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
+
+            return workbook;
         }
     }
 }
